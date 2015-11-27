bc73b1360593388b4b496686633d54744be4bc96
Norman Rink <norman.rink@tu-dresden.de>
2015-11-27 16:43:08 +0100

    Adapted the 'bubblesort.random-binary' test case to the new build system.

--- tests/bubblesort.random-binary/bubblesort.random-binary.enc.c
+++ tests/bubblesort.random-binary/bubblesort.random-binary.enc.c
@@ -1,28 +1,28 @@
 
-#include "encode.h"
+long ___enc_bubblesort(long *, long) __attribute__((noinline));
 
 long ___enc_bubblesort(long *array, long size) {
-    long swapped;
-    long i;
-    for (i = 1; i < size; i++)
-    {
-        swapped = 0;    //this flag is to check if the array is already sorted
-        long j;
-        for(j = 0; j < size - i; j++)
-        {
-            if(array[j] > array[j+1])
-            {
-                long temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-                swapped = 1;
-            }
-        }
-        if(!swapped){
-            break; //if it is sorted then stop
-        }
-    }
-    AN_CHECK_RANGE(array, size);
-    return 0;
- }
+  long swapped;
 
+  for (long i = 1; i < size; i++) {
+    swapped = 0;    //this flag is to check if the array is already sorted
+
+    for (long j = 0; j < size - i; j++) {
+      if (array[j] > array[j+1]) {
+        long temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+        swapped = 1;
+      }
+    }
+    if (!swapped) {
+      break; //if it is sorted then stop
+    }
+  }
+  return 0;
+}
+
+
+long ___enc_get(long *a, long i) {
+  return a[i];
+}
