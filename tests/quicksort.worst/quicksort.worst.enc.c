dd05d96ebb6161cbed96f55ebd7acb08180fd893
Norman Rink <norman.rink@tu-dresden.de>
2015-11-27 16:59:16 +0100

    Adapted the 'quicksort.worst' test case to the new build system.

--- tests/quicksort.worst/quicksort.worst.enc.c
+++ tests/quicksort.worst/quicksort.worst.enc.c
@@ -1,39 +1,42 @@
 
-#include "encode.h"
+void quicksort(long *, long, long) __attribute__((noinline));
 
 void quicksort(long *array, long start, long end) {
-    if (start < end) {
-        long l = start+1, r = end, p = array[start];
+  if (start < end) {
+    long l = start+1, r = end, p = array[start];
 
-        while(l < r) {
-            if(array[l] <= p) {
-                l++;
-            } else if(array[r] >= p) {
-                r--;
-            } else {
-                long t = array[l];
-                array[l] = array[r];
-                array[r] = t;
-            }
-        }
-
-        if(array[l] < p) {
-            long t = array[l];
-            array[l] = p;
-            array[start] = t;
-            l--;
-        } else {
-            l--;
-            long t = array[l];
-            array[l] = p;
-            array[start] = t;
-        }
-        quicksort(array, start, l);
-        quicksort(array, r, end);
+    while (l < r) {
+      if (array[l] <= p) {
+        l++;
+      } else if(array[r] >= p) {
+        r--;
+      } else {
+        long t = array[l];
+        array[l] = array[r];
+        array[r] = t;
+      }
     }
+
+    if (array[l] < p) {
+      long t = array[l];
+      array[l] = p;
+      array[start] = t;
+      l--;
+    } else {
+      l--;
+      long t = array[l];
+      array[l] = p;
+      array[start] = t;
+    }
+    quicksort(array, start, l);
+    quicksort(array, r, end);
+  }
 }
 
 void ___enc_quicksort(long *array, long start, long end) {
   quicksort(array, start, end);
-  AN_CHECK_RANGE(array, end-start);
+}
+
+long ___enc_get(long *a, long i) {
+  return a[i];
 }
