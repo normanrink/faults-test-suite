8c6e55222ac31c501af8201314bfccadce57c26f
Norman Rink <norman.rink@tu-dresden.de>
2015-11-27 16:26:53 +0100

    Adapted the 'quicksort.best' test case to the new build system.

---
+++ tests/quicksort.best/generate.c
@@ -0,0 +1,33 @@
+/* This program is for generating a C source file that contains the
+   input array. Hence this file is not part of the usual build system.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define SIZE 32000
+
+long array[SIZE];
+
+
+int main() {
+  FILE *out = fopen("quicksort.best.input.c", "w");
+
+  srand(0);
+  for (unsigned long i = 0; i < SIZE; i++) {
+    array[i] = i;
+  }
+
+  for (unsigned a = 0; a < 2; a++) {
+    fprintf(out, "\nlong a%s[%u] = {", (a == 0) ? "0" : "1", SIZE);
+    for (unsigned i = 0; i < SIZE; i++) {
+      fprintf(out, "%s0x%08lx,", (i % 10) ? " " : "\n", array[i]);
+    }
+    fprintf(out, "\n};\n");
+  }
+
+  fclose(out);
+
+  return 0;
+}
