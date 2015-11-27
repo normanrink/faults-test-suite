ca84049e7bb59e0093392e61a249b4dcd742055a
Norman Rink <norman.rink@tu-dresden.de>
2015-11-27 16:50:17 +0100

    Adapted the 'quicksort.random-binary' test case to the new build system.

--- tests/quicksort.random-binary/quicksort.random-binary.main.c
+++ tests/quicksort.random-binary/quicksort.random-binary.main.c
@@ -2,50 +2,71 @@
    "AN-Transformer".
  */
 
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-#include "encode.h"
 #include "mycheck.h"
 #include "mycyc.h"
 
-void ___enc_quicksort(long*, long, long);
+
+#define SIZE 32000
+
+const unsigned length = (LENGTH < SIZE) ? LENGTH : SIZE;
+const unsigned repetitions = REPETITIONS;
+
+extern void ___enc_quicksort(long*, long, long);
+extern long ___enc_get(long *, long);
+
+
+extern long a0[length];
+extern long a1[length];
+
 
 int main(int argc, char **argv) {
-    long *a;
-    uint64_t t1, t2, total = 0;
-    unsigned i, j;
-    const unsigned length = LENGTH;
-    const unsigned repetitions = REPETITIONS;
+  uint64_t t1, t2, total = 0;
 
-    fprintf(stderr, "LENGTH=%d\n", length);
-    fprintf(stderr, "REPETITIONS=%d\n", repetitions);
+#ifdef DEBUG
+  fprintf(stderr, "LENGTH=%d\n", length);
+  fprintf(stderr, "REPETITIONS=%d\n", repetitions);
+#endif
 
-    __cs_log(argc, argv);
-    __cs_fopen(argc, argv);
-    __cs_reset();
+  __cs_fopen(argc, argv);
+#if (defined DEBUG) || (defined CHECKSUM)
+  __cs_log(argc, argv);
+  __cs_reset();
+#endif
 
-    a = (long*)malloc(length * sizeof(long));
-
-    srand(0);
-    for (i = 0; i < repetitions; i++) {
-      for (j = 0; j < length; j++)
-        a[j] = AN_ENCODE_VALUE((rand() % 2));
-
-      __cyc_warmup();
-      t1 = __cyc_rdtsc();
-      ___enc_quicksort(a, 0, length-1);
-      t2 = __cyc_rdtscp();
-      total += t2 - t1;
-
-      for (j = 0; j < length; j++) {
-        __cs_facc(AN_DECODE_VALUE(a[j]));
-        __cs_acc(AN_DECODE_VALUE(a[j]));
-      }
+  for (unsigned i = 0; i < repetitions; i++) {
+    if (i > 0) {
+      // Sorting takes place "in-situ". Therefore, after the first
+      // repetition, the initial contents in 'a0' must be restored.
+      // (The 'memcpy' is obviously never executed if there is only
+      // one repetition. This is intended to speed up fault injection
+      // runs.)
+      memcpy(&a0[0], &a1[0], sizeof(long)*length);
     }
 
-    __cyc_msg(total);
-    __cs_fclose();
-    __cs_msg();
+    __cyc_warmup();
+    t1 = __cyc_rdtsc();
+    ___enc_quicksort(&a0[0], 0, length-1);
+    t2 = __cyc_rdtscp();
+    total += t2 - t1;
 
-    return 0;
+    for (unsigned j = 0; j < length; j++) {
+      __cs_facc(___enc_get(a0, j));
+#if (defined DEBUG) || (defined CHECKSUM)
+      __cs_acc(___enc_get(a0, j));
+#endif
+    }
+  }
+
+  __cs_fclose();
+#if (defined DEBUG) || (defined CYCLES)
+  __cyc_msg(total);
+#endif
+#if (defined DEBUG) || (defined CHECKSUM)
+  __cs_msg();
+#endif
+
+  return 0;
 }
